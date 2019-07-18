ae9204fc2131e86de32978d0b05ddb722963c873
zaytsev <zaytsev@srfstudio.ru>
2019-07-18 10:59:23 +0300

    add pull request job/ build only template

---
+++ ci-internal/script/build_repo.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+ios=ios
+android=android
+
+platform=
+
+function build() {
+    if [[ ${platform} = ${ios} ]]; then
+       flutter build ios
+    else
+       flutter build apk
+    fi
+}
+
+function buildRepo() {
+#    build template only todo change to modules when examples are created
+
+    cd ./template
+    build
+    cd ..
+
+    #todo uncomment in future
+#    for dir in ./* ; do
+#        pwd
+#
+#        echo ${dir}
+#        cd ${dir}
+#        build
+#        cd ..
+#    done
+}
+
+
+echo "Parameters" $1 $2
+while [[ -n "$1" ]]; do # while loop starts
+
+        case "$1" in
+
+            -ios )          platform=ios
+                            ;;
+
+            -android )      platform=android
+                            ;;
+
+            esac
+
+         shift
+
+done
+
+buildRepo
