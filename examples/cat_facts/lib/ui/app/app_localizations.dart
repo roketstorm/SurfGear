544a8d4d22698d7528c8ccfda0a0f41dd9e6dabf
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-07-20 18:22:52 +0300

    step last

--- examples/cat_facts/lib/ui/app/app_localizations.dart
+++ examples/cat_facts/lib/ui/app/app_localizations.dart
@@ -1,6 +1,6 @@
+import 'package:cat_facts/l10n/messages_all.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 
 class AppLocalizations {
   static const Iterable<Locale> supportedLocales = [
@@ -17,6 +17,8 @@ class AppLocalizations {
     final canonicalLocaleName = Intl.canonicalizedLocale(localeName);
 
     Intl.defaultLocale = canonicalLocaleName;
+
+    await initializeMessages(canonicalLocaleName);
 
     return AppLocalizations();
   }
